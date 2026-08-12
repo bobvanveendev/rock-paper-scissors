592169b894e4e492f59588375bf606d2d8d9063d
Bob van Veen <bobvanveen@kpnmail.nl>
2026-08-12 21:15:14 +0200

    Added human choice and play game function

--- script.js
+++ script.js
@@ -1,5 +1,73 @@
 function getComputerChoice(max) {
-  return Math.floor(Math.random() * max);
+  let result = Math.floor(Math.random() * max);
+  if (result === 0) {
+    return "rock";
+  } else if (result === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
-console.log(getComputerChoice(3));
+function getHumanChoice() {
+  let choice = prompt("Enter your choice: rock, paper or scissors");
+  return choice.toLowerCase();
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      humanScore++;
+      console.log("You win! Rock beats scissors.");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      humanScore++;
+      console.log("You win! Paper beats rock.");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      humanScore++;
+      console.log("You win! Scissors beats paper.");
+    } else {
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    }
+  }
+
+  let humanSelection = getHumanChoice();
+  let computerSelection = getComputerChoice(3);
+
+  playRound(humanSelection, computerSelection);
+
+  humanSelection = getHumanChoice();
+  computerSelection = getComputerChoice(3);
+
+  playRound(humanSelection, computerSelection);
+
+  humanSelection = getHumanChoice();
+  computerSelection = getComputerChoice(3);
+
+  playRound(humanSelection, computerSelection);
+
+  humanSelection = getHumanChoice();
+  computerSelection = getComputerChoice(3);
+
+  playRound(humanSelection, computerSelection);
+
+  humanSelection = getHumanChoice();
+  computerSelection = getComputerChoice(3);
+
+  playRound(humanSelection, computerSelection);
+
+  if (humanScore > computerScore) {
+    console.log("Human wins");
+  } else if (computerScore > humanScore) {
+    console.log("Computer wins");
+  } else {
+    console.log("It's a draw");
+  }
+}
+
+playGame();
